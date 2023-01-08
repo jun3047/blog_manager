87e9ede7d2598051f1e810361a579344f63c8be7
jeong jun <jungjunjenon3047@gmail.com>
2023-01-08 20:13:47 +0900

    basic design and react

---
+++ blog-manager/src/component/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Header = () => {
+    return(<header>
+    <div className="logo">O-O</div>
+    <div className="flex-box"></div>
+    <div className="meun">
+        <div className="overview">소개</div>
+        <div className="link-roll">링크 등록</div>
+        <div className="login">로그인</div>
+    </div>
+    </header>)
+}
+
+export default Header;
